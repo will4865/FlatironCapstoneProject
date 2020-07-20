1f9875862e1875ee5058bacd576184dc539b82f9
William Newton <will4856@gmail.com>
2020-07-20 16:50:49 -0400

    re cleaned scripts using text hero, made word cloud

--- functions/lemmatize_token.py
+++ functions/lemmatize_token.py
@@ -1,11 +1,10 @@
-def clean_lemmatize_token(script):
+def lemmatize_token(script):
     """Function used to automatically remove stop words, twitter_specific words and punctuation, tokenize, lemmatize
     and join together words into one string before modeling"""
-    cleaned = script.translate(str.maketrans('', '', string.punctuation)).lower()
-    tokenized = word_tokenize(cleaned)
+    tokenized = word_tokenize(script)
     lemmatizer = WordNetLemmatizer()
     lemmatized = []
     for word in tokenized:
         lemmatized.append(lemmatizer.lemmatize(word))
-    lemmatized = ' '.join(lemmatized)
+    #lemmatized = ' '.join(lemmatized)
     return lemmatized
